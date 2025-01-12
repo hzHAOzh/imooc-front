7f6ffc4b9de31e55d3bdc783dc201912d5d7da5b
hzHAOzh <a953648635@163.com>
2025-01-13 00:24:21 +0800

    feat: 移动端slider处理

--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -3,15 +3,17 @@
 </template>
 
 <script setup>
-import { isMobileTerminal } from '@/utils/flexible'
-import mobileNavigation from './mobile/index.vue'
 import { getCategory } from '@/api/category'
+import { ALL_CATEGORY_ITEM } from '@/constants'
+import { isMobileTerminal } from '@/utils/flexible'
 import { ref } from 'vue'
+import mobileNavigation from './mobile/index.vue'
 
 const categoryData = ref([])
 const getCategoryData = async () => {
   const { categorys } = await getCategory()
   categoryData.value = categorys
+  categoryData.value.unshift(ALL_CATEGORY_ITEM)
 }
 getCategoryData()
 </script>
--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -2,16 +2,27 @@
   <div class="bg-white sticky top-0 left-0 z-10">
     <ul
       class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
+      ref="ulTarget"
     >
+      <li
+        ref="sliderTarget"
+        class="absolute h-[22px] bg-zinc-900 rounded-lg duration-200"
+        :style="sliderStyle"
+      ></li>
       <li
         class="fixed top-0 right-0 h-4 px-1 flex items-center bg-white dark:bg-zinc-900 z-20 shadow-l-white dark:shadow-l-zinc"
       >
         <m-svg-icon class="w-1.5 h-1.5" name="hamburger" />
       </li>
       <li
-        v-for="item in data"
+        v-for="(item, index) in data"
+        :ref="setItemRef"
         :key="item.id"
+        @click="onItemClick(index)"
         class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
+        :class="{
+          'text-zinc-100': currentCategoryIndex === index
+        }"
       >
         {{ item.name }}
       </li>
@@ -20,12 +31,48 @@
 </template>
 
 <script setup>
+import { useScroll } from '@vueuse/core'
+import { onBeforeUpdate, ref, watch } from 'vue'
 defineProps({
   data: {
     type: Array,
     required: true
   }
 })
+
+// 滑块
+const sliderStyle = ref({
+  transform: 'translateX(0px)',
+  width: '52px'
+})
+// 选中的item下标
+const currentCategoryIndex = ref(0)
+// 获取填充的所有item元素
+let itemRefs = []
+const setItemRef = (el) => {
+  if (el) {
+    itemRefs.push(el)
+  }
+}
+onBeforeUpdate(() => {
+  itemRefs = []
+})
+// 获取ul元素，计算偏移位置
+const ulTarget = ref(null)
+const { x: ulScrollLeft } = useScroll(ulTarget)
+watch(currentCategoryIndex, (val) => {
+  // 获取选中元素的left、width
+  const { left, width } = itemRefs[val].getBoundingClientRect()
+  // 为sliderStyle设置属性
+  sliderStyle.value = {
+    transform: `translateX(${ulScrollLeft.value + left - 10}px)`,
+    width: width + 'px'
+  }
+})
+// item 点击事件
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index
+}
 </script>
 
 <style lang="scss" scoped></style>
